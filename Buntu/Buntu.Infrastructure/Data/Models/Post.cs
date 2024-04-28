aa7e1f98bdd7b4384210cb963969e80cb1f15e86
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-28 16:17:07 +0000

    Created Post entity

---
+++ Buntu/Buntu.Infrastructure/Data/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Buntu.Infrastructure.Data.Models
+{
+    [Comment("Post")]
+    public class Post
+    {
+        [Key]
+        [Comment("Post identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Post content")]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Publisher identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Date of creation")]
+        public DateTime CreatedDate { get; set; }
+
+        [Comment("Post image")]
+        public byte[] Image { get; set; } = new byte[128];
+
+        [Required]
+        [Comment("Post status")]
+        public string Status { get; set; } = string.Empty;
+    }
+}
