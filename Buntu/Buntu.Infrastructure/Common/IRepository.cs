1b521db3f9c09acbb8e642a37825b27f485d1bcc
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-29 20:36:53 +0000

    Created Repository interface

---
+++ Buntu/Buntu.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Buntu.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> AllReadOnly<T>();
+        Task AddAsync<T>(T entity) where T : class;
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+        Task DeleteAsync<T>(object id) where T: class;
+        Task<int> SaveChangesAsync();
+    }
+}
