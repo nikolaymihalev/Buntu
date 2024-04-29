470a22f94261ad7522dd67de0fcc4a128d4276ae
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-29 10:29:45 +0000

    Created Notification entity

---
+++ Buntu/Buntu.Infrastructure/Data/Models/Notification.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Buntu.Infrastructure.Data.Models
+{
+    [Comment("Notification")]
+    public class Notification
+    {
+        [Comment("Notification identifier")]
+        public int Id { get; set; }
+
+        [Comment("User identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [Comment("Notification type")]
+        public string Type { get; set; } = string.Empty;
+
+        [Comment("Related type identifier (like, comment, follow)")]
+        public object RelatedId { get; set; } = null!;
+
+        [Comment("Is notification read")]
+        public bool IsRead { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
