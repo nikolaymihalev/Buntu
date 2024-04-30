bff391a16d7e16581c34327ce827f74a9e18d584
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-30 10:05:33 +0000

    Created Post service interface with class

---
+++ Buntu/Buntu.Core/Contracts/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using Buntu.Core.Models.Post;
+
+namespace Buntu.Core.Contracts
+{
+    public interface IPostService
+    {
+        Task<PostPageModel> GetPostsForPageAsync(int currentPage = 1);
+        Task<IEnumerable<PostInfoModel>> GetAllPostsAsync();
+        Task AddPostAsync(PostFormModel model);
+        Task EditPostAsync(int id);
+        Task DeletePostAsync(int id);
+        Task<PostInfoModel?> GetPostByIdAsync(int id);
+    }
+}
---
+++ Buntu/Buntu.Core/Services/PostService.cs
@@ -0,0 +1,38 @@
+﻿using Buntu.Core.Contracts;
+using Buntu.Core.Models.Post;
+
+namespace Buntu.Core.Services
+{
+    public class PostService : IPostService
+    {
+        public Task AddPostAsync(PostFormModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePostAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPostAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PostInfoModel>> GetAllPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostInfoModel?> GetPostByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostPageModel> GetPostsForPageAsync(int currentPage = 1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
