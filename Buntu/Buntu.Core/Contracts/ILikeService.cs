133ac6270e20a9482a15b055417d572a02ac31e5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-01 06:55:53 +0000

    Created Interface for Like service

---
+++ Buntu/Buntu.Core/Contracts/ILikeService.cs
@@ -0,0 +1,13 @@
+﻿using Buntu.Core.Models.Like;
+
+namespace Buntu.Core.Contracts
+{
+    public interface ILikeService
+    {
+        Task AddLikeAsync(LikeAddModel model);
+        Task EditLikeAsync(LikeAddModel model);
+        Task RemoveLikeAsync(int id);
+        Task<LikeInfoModel?> GetLikeByIdAsync(int id);
+        Task<IEnumerable<LikeInfoModel>> GetLikesForPostAsync(int postId);
+    }
+}
