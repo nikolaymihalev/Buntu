ff02ca86968f7e052c4332faca339b2963635aae
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-01 07:19:41 +0000

    Created interface for Follow service

---
+++ Buntu/Buntu.Core/Contracts/IFollowService.cs
@@ -0,0 +1,11 @@
+﻿using Buntu.Core.Models.Follow;
+
+namespace Buntu.Core.Contracts
+{
+    public interface IFollowService
+    {
+        Task AddFollowerAsync(FollowInfoModel model);
+        Task RemoveFollowerAsync(int id);
+        Task<IEnumerable<FollowInfoModel>> GetUserFollowersAsync(string userId);
+    }
+}
