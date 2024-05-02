315e3dcd45e79a57c09771f400d3ca8ec33ec2ad
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-02 07:28:35 +0000

    Created interface for Notification Service

---
+++ Buntu/Buntu.Core/Contracts/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using Buntu.Core.Models.Notification;
+
+namespace Buntu.Core.Contracts
+{
+    public interface INotificationService
+    {
+        Task AddNotificationAsync(NotificationModel model);
+        Task DeleteNotificationAsync(int id);
+        Task MarkNotificationAsReadAsync(int id);
+        Task<IEnumerable<NotificationModel>> GetUserNotificationsAsync(string userId);
+    }
+}
