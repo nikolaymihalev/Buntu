a835eecfffcaa4736e5ca4754adfc74e1cbc4bad
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-03 13:46:23 +0000

    Updated like view

--- Buntu/Buntu/wwwroot/js/site.js
+++ Buntu/Buntu/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.querySelector('.like-btn').addEventListener('click', function () {
+    document.querySelector('.reaction-form').style.display = 'block';
+});
