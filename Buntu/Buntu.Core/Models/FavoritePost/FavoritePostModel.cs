e65656c55caa360d2531e9584174371cd373ea47
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-04 19:53:26 +0000

    Created Favorite post dto model

---
+++ Buntu/Buntu.Core/Models/FavoritePost/FavoritePostModel.cs
@@ -0,0 +1,30 @@
+﻿using Buntu.Core.Models.Post;
+
+namespace Buntu.Core.Models.FavoritePost
+{
+    /// <summary>
+    /// Model for information about user favorite post
+    /// </summary>
+    public class FavoritePostModel
+    {
+        /// <summary>
+        /// Favorite post identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Post identifier
+        /// </summary>
+        public int PostId { get; set; }
+
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Post model
+        /// </summary>
+        public PostInfoModel Post { get; set; }
+    }
+}
