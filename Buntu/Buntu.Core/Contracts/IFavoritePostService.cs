8f177b2861387eef66f44a6a3f67485f4949033e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-05 12:33:03 +0000

    Created interface for Favorite post service

---
+++ Buntu/Buntu.Core/Contracts/IFavoritePostService.cs
@@ -0,0 +1,11 @@
+﻿using Buntu.Core.Models.FavoritePost;
+
+namespace Buntu.Core.Contracts
+{
+    public interface IFavoritePostService
+    {
+        Task AddFavoriteAsync(FavoritePostModel model);
+        Task RemoveFavoriteAsync(int id);
+        Task<IEnumerable<FavoritePostModel>> GetUserFavoritePostsAsync(string userId);
+    }
+}
