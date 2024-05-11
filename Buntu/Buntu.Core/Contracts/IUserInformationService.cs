99f958faaed4e96f7ba275a85bb6236da84378b8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-05-11 11:21:12 +0000

    Created interface for User information service

---
+++ Buntu/Buntu.Core/Contracts/IUserInformationService.cs
@@ -0,0 +1,10 @@
+﻿using Buntu.Core.Models.UserInformation;
+
+namespace Buntu.Core.Contracts
+{
+    public interface IUserInformationService
+    {
+        Task AddUserInformationAsync(UserInformationModel model);
+        Task<UserInformationModel> GetUserInformationAsync(string userId);
+    }
+}
